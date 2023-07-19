f4c27be850a7dc10c3fa6b287c5036b3cbe7a7e5
Mavericks Balitaan <balitaanmavericks@gmail.com>
2023-07-19 06:43:49 +0500

    Use cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const server = express();
@@ -12,13 +13,13 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 server.use(express.json());
+server.use(cors());
 server.use("/api", bookRouter);
 
 // home route
 server.get("/", (req, res) => {
   res.send("Welcome to Express Server");
 });
-
 
 server.listen(port, () => {
   db.connectToServer().then(() => {
